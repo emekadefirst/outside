4f9fce7f507d9a80cd7fae764f45593e9971ef5f
emekadefirst <victorchibuogwu33@gmail.com>
2025-02-06 23:27:47 +0100

    requirements.txt

--- api/payment/database.py
+++ api/payment/database.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 
 DATABASE_URL = os.getenv("POSTGRESQL")
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=True)
 
 
 def create_db_and_tables():
